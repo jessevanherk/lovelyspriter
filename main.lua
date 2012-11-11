4c2238da7e7de12d24ec3fcdeecc3abd3ca76f3c
Ben Humphreys <benhumphreys@gmail.com>
2012-11-11 23:15:55 +0900

    First decent commit

---
+++ main.lua
@@ -0,0 +1,27 @@
+local lg = love.graphics
+
+require("spriter")
+
+function love.load()
+  spriter = Spriter:new('testrun/testrun.scml', 'testrun/')
+  runAnim = spriter:getAnim("run")
+
+  local w, h, f, v, a = lg.getMode()
+  local success = lg.setMode(w, h, f, not v, a)
+  if not success then
+    print("ERROR: Failed to toggle VSync")
+    lg.setMode(w, h, f, v, a)
+  end
+end
+
+function love.update(dt)
+  runAnim:update(dt)
+end
+
+function love.draw()
+  runAnim:draw(350, 350)
+
+  -- showfps
+  lg.setColor(255,0,0,255)
+  lg.print(tostring(love.timer.getFPS()), lg.getWidth()-30, lg.getHeight()-20)
+end
